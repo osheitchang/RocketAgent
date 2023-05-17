e2bea26d8231a45d7afc23c2d07b4c2005c6da40
Jonás Dias <jonasuneg@gmail.com>
2023-05-17 15:36:46 -0500

    finish firebase integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
+import React, {useState, useEffect} from "react";
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import axios from "axios";
-
 
 import Header from '../src/components/Header';
 import Hero from '../src/components/Hero';
@@ -11,14 +11,31 @@ import SecondFeature from '../src/components/SecondFeature';
 import Pricing from '../src/components/Pricing';
 import Form from '../src/components/Form'
 import Newsletter from '../src/components/Newsletter'
-import {ProductModel} from "@/pages/api/products/product.model";
+import {ProductModel} from "@/src/model/product.model";
 import env from "@/pages/api/config";
+import {AlertModel} from "@/src/model/alert.model";
 
 axios.defaults.baseURL = env.API_BASE;
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = ({products}: {products: ProductModel[]}) => {
+  const [alert, setAlert] = useState<AlertModel>({
+    show: false,
+    title: '',
+    description: '',
+    type: 'success',
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert({
+        show: false,
+        title: '',
+        description: '',
+        type: 'success',
+      });
+    }, 2000);
+  }, [alert.show]);
   return (
     <>
       <Head>
@@ -35,8 +52,27 @@ const Home = ({products}: {products: ProductModel[]}) => {
         <SecondFeature />
         <Pricing products={products} />
         <Newsletter />
-        <Form />
+        <Form onSuccessSubmit={setAlert} />
       </main>
+      {alert.show && (
+        <div className="alert-container">
+          <div className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+            <div className="flex">
+              <div className="py-1">
+                <svg className="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 20 20">
+                  <path
+                    d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+                </svg>
+              </div>
+              <div>
+                <p className="font-bold">{alert.title}</p>
+                <p className="text-sm">{alert.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <Footer />
     </>
   )
--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,17 +1,5 @@
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
-import env from "@/pages/api/config";
+import app from '@/src/utils/firebase/initialize';
+import {getFirestore} from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: env.FIREBASE_API_KEY,
-  authDomain: env.FIREBASE_AUTH_DOMAIN,
-  projectId: env.FIREBASE_PROJECT_ID,
-  storageBucket: env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.FIREBASE_APP_ID,
-  measurementId: env.FIREBASE_MEASUREMENT_ID,
-};
-
-initializeApp(firebaseConfig);
-const firestore = getFirestore();
+const firestore = getFirestore(app);
 export default firestore;
